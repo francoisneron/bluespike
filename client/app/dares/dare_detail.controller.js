1b424599c5a26e7e7c90699d194f6aa340bbeda4
Emile Cantin <emile.cantin@gmail.com>
2015-05-15 00:12:27 -0400

    Facebook share.

--- client/app/dares/dare_detail.controller.js
+++ client/app/dares/dare_detail.controller.js
@@ -1,8 +1,26 @@
 'use strict';
 
-angular.module('dareApp').controller('DareDetailCtrl', function ($scope, $stateParams, $state, Dare, Video, LoginService) {
+angular.module('dareApp').controller('DareDetailCtrl', function ($scope, $stateParams, $state, Dare, Video, LoginService, $facebook, $location) {
   $scope.dare = Dare.get({id: $stateParams.key});
   $scope.videos = Video.query({dare: $stateParams.key});
+  $scope.send_dare = function(dare) {
+    $facebook.ui({
+      method: 'share_open_graph',
+      action_type: 'dare_dev:send',
+      action_properties: JSON.stringify({
+          challenge:{
+            'og:url': $location.absUrl(),
+            'og:title': dare.name,
+            'og:type': 'dare_dev:challenge',
+            'og:image': dare.image,
+            'og:description': dare.info,
+            'fb:app_id': 420157531503239
+          }
+      })
+    }, function(response){
+      // nothing to do here, mobe along
+    });
+  };
   $scope.do_dare = function(dare) {
     LoginService.ensureLoggedIn().then(function(currentUser) {
       var video = new Video({dare: dare.key, user: currentUser.email});
@@ -10,6 +28,5 @@ angular.module('dareApp').controller('DareDetailCtrl', function ($scope, $stateP
     }).then(function() {
       $state.go('my_dares');
     });
-
-  }
+  };
 });
