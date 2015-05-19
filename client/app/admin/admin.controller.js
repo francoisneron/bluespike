1082c60ca9506f897af67e7f0c79e8b97d462176
Emile Cantin <emile.cantin@gmail.com>
2015-05-19 17:42:25 -0400

    Added way to add a Dare. Filtered the fields in the admin edit form,

--- client/app/admin/admin.controller.js
+++ client/app/admin/admin.controller.js
@@ -1,16 +1,21 @@
 'use strict';
 
 angular.module('dareApp').controller('AdminCtrl', function ($scope, $http, $modal) {
+  var filterKeys = function(item) {
+    return Object.keys(item).filter(function(key) {
+      return key !== '__v' && key !== '_id' && key !== '$$hashKey';
+    });
+  };
   $scope.edit = function(item) {
-    console.log(Object.keys(item));
     var modal = $modal.open({
       templateUrl: 'app/admin/editModal.html',
       controller: function($scope, fields, item) {
         $scope.fields = fields;
+        console.log(fields);
         $scope.item = item;
       },
       resolve: {
-        fields: function() {return Object.keys(item);},
+        fields: function() {return filterKeys(item);},
         item: function() {return item;}
       }
     });
@@ -20,4 +25,5 @@ angular.module('dareApp').controller('AdminCtrl', function ($scope, $http, $moda
       }
     });
   };
+
 });
--- client/app/admin/dares/dares.controller.js
+++ client/app/admin/dares/dares.controller.js
@@ -1,17 +1,30 @@
 'use strict';
 
-angular.module('dareApp')
-  .controller('DaresAdminCtrl', function ($scope, $http, Auth, Dare) {
+angular.module('dareApp').controller('DaresAdminCtrl', function ($scope, $http, Auth, Dare, $modal) {
 
-    // Use the User $resource to fetch all items
-    $scope.items = Dare.query();
+  $scope.items = Dare.query();
 
-    $scope.delete = function(item) {
-      Dare.remove({ id: item._id });
-      angular.forEach($scope.items, function(u, i) {
-        if (u === item) {
-          $scope.items.splice(i, 1);
-        }
-      });
-    };
-  });
+  $scope.delete = function(item) {
+    Dare.remove({ id: item._id });
+    angular.forEach($scope.items, function(u, i) {
+      if (u === item) {
+        $scope.items.splice(i, 1);
+      }
+    });
+  };
+
+  $scope.create = function() {
+    var modal = $modal.open({
+      templateUrl: 'app/admin/editModal.html',
+      controller: function($scope) {
+        $scope.item = {};
+        $scope.fields = ["key", "name", "info", "active", "image"];
+      }
+    });
+    modal.result.then(function(item) {
+      item = new Dare(item);
+      item.$save();
+      $scope.items.push(item);
+    });
+  };
+});
